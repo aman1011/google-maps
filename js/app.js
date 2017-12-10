85663f829f449abaf3113d68700762f7b050fa75
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-10 18:03:23 +0530

    Adding the list of myPlaces in app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var myPlaces = [
+{
+	name: 'The Albert',
+	Address: '185 Walton Breck Rd, Liverpool L4 0RE, UK'
+},
+{
+	name: 'Arkles',
+	Address: '77 Anfield Rd, Liverpool L4 0TJ, UK'
+},
+{
+	name: 'The Sandon',
+	Address: '178-182 Oakfield Rd, Liverpool L4 0UH, UK'
+},
+{
+	name: 'The Park Pub',
+	Address: '216-218 Walton Breck Rd, Liverpool L4 0RQ, UK'
+},
+{
+	name: 'The Twelfth Man',
+	Address: '121 Walton Breck Rd, Liverpool L4 0RD, UK'
+}
+];
