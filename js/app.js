9ebb19d2df81656b2aaf97f1ad229d064c672694
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-12 18:42:52 +0530

    Revert "fixing the recurring bug"
    This reverts commit ea318fd0ef074e581046588fec01dd09c12afd7c.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ var ViewModel = function (googleMap, myPlaces, infoWindow, bounds) {
 	this.map = googleMap;
 	this.allPlaces = ko.observableArray([]);
 	this.markers = [];
-	this.infoWindow = infoWindow;
 	var geocoder = new google.maps.Geocoder();
     myPlaces.forEach(function(place) {
     	var newObj = new Place(place);
@@ -27,7 +26,7 @@ var ViewModel = function (googleMap, myPlaces, infoWindow, bounds) {
 
         		(function (marker, title) {
                      google.maps.event.addListener(marker, 'click', function () {
-                         populateinfoWindow(marker, self.infoWindow);
+                         populateinfoWindow(marker, infoWindow);
                      });
                  })(marker, title);
 
@@ -56,7 +55,7 @@ var ViewModel = function (googleMap, myPlaces, infoWindow, bounds) {
 
         		(function (marker, title) {
                      google.maps.event.addListener(marker, 'click', function () {
-                         populateinfoWindow(marker, self.infoWindow);
+                         populateinfoWindow(marker, infoWindow);
                      });
                  })(marker, place.name);
 
@@ -142,7 +141,6 @@ function populateinfoWindow(marker, infoWindow) {
 		       	
    	// Check to make sure the infoWindow is not already opened on this marker.
     if (infoWindow.marker != marker) {
-    	
    		infoWindow.marker = marker;
       	infoWindow.setContent('<div id="text">' + marker.title + '</div>' + '<div id="text">' + marker.address + '</div>');
       	infoWindow.open(map, marker);
@@ -150,7 +148,6 @@ function populateinfoWindow(marker, infoWindow) {
       	// Make sure the marker property is cleared if the infoWindow is closed.
       	infoWindow.addListener('closeclick',function(){
         	infoWindow.setMarker = null;
-        	infoWindow.marker = null;
       	});
 
       	// Get the street View for the place.
@@ -186,5 +183,20 @@ function populateinfoWindow(marker, infoWindow) {
       	// Calling the above function with the marker data.
       	streetView.getPanoramaByLocation(marker.position, radius, getStreetView);
       	infoWindow.open(map, marker);
+
+      	// Making the ajax call from foursquare to get the open time
+      	// of the pub.
+      	console.log(marker.position.lat());
+        var request_url = 'https://api.foursquare.com/v2/venues/search?ll=53.4301536,-2.9625996&client_id=KXTPUTXWJXUUUE22RHHQ3YNYEGZHBG31AXS0CFOHWL3AHANU&client_secret=3IF050LBAUYMCD1UV55HV2IAA1WOLR3NFMWYOAVYUVCNU5U2&v=20171127'
+        $.ajax({
+        	url: request_url,
+        	dataType: 'jsonp',
+        	success: function(response) {
+        		console.log('success');
+        	}
+        }).fail(function(e) {
+        	console.log('failed');
+        });
+
     }
 }
