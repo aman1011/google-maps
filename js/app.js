fa9f7f49f9e4997b63265a44adce59c01039c103
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-14 00:52:28 +0530

    tab and adding googleError function

--- js/app.js
+++ js/app.js
@@ -151,36 +151,41 @@ var createMap = function () {
 }
 
 function initMap() {
-google.maps.event.addDomListener(window, 'load', function(){
+	google.maps.event.addDomListener(window, 'load', function(){
 
-	// list of my places.
-	var myPlaces = [
-	{
-		name: 'The Albert',
-		address: '185 Walton Breck Rd, Liverpool L4 0RE, UK'
-	},
-	{
-		name: 'Arkles',
-		address: '77 Anfield Rd, Liverpool L4 0TJ, UK'
-	},
-	{
-		name: 'The Sandon',
-		address: '178-182 Oakfield Rd, Liverpool L4 0UH, UK'
-	},
-	{
-		name: 'The Park Pub',
-		address: '216-218 Walton Breck Rd, Liverpool L4 0RQ, UK'
-	},
-	{
-		name: 'The Twelfth Man',
-		address: '121 Walton Breck Rd, Liverpool L4 0RD, UK'
-	}
-	];
-	var googleMap = createMap();
-	var infoWindow = new google.maps.InfoWindow();
-	var bounds = new google.maps.LatLngBounds();
-	ko.applyBindings(new ViewModel(googleMap, myPlaces, infoWindow, bounds))
-});
+		// list of my places.
+		var myPlaces = [
+		{
+			name: 'The Albert',
+			address: '185 Walton Breck Rd, Liverpool L4 0RE, UK'
+		},
+		{
+			name: 'Arkles',
+			address: '77 Anfield Rd, Liverpool L4 0TJ, UK'
+		},
+		{
+			name: 'The Sandon',
+			address: '178-182 Oakfield Rd, Liverpool L4 0UH, UK'
+		},
+		{
+			name: 'The Park Pub',
+			address: '216-218 Walton Breck Rd, Liverpool L4 0RQ, UK'
+		},
+		{
+			name: 'The Twelfth Man',
+			address: '121 Walton Breck Rd, Liverpool L4 0RD, UK'
+		}
+		];
+		var googleMap = createMap();
+		var infoWindow = new google.maps.InfoWindow();
+		var bounds = new google.maps.LatLngBounds();
+		ko.applyBindings(new ViewModel(googleMap, myPlaces, infoWindow, bounds))
+	});
+}
+
+// google Error function.
+function googleError() {
+	alert("Google Map API's could not be loaded");
 }
 
 function populateinfoWindow(marker, infoWindow) {
