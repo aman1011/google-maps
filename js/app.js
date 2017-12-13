1a0b70c670e80581daf8946cab0c26b106972c5b
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-14 01:04:33 +0530

    fixing and tabbing

--- js/app.js
+++ js/app.js
@@ -11,10 +11,11 @@ var ViewModel = function (googleMap, myPlaces, infoWindow, bounds) {
 	this.userInput = ko.observable('');
 	this.visiblePlaces = ko.observableArray([]);
 	var geocoder = new google.maps.Geocoder();
+	var errFlag = false;
 	myPlaces.forEach(function(place) {
 		var newObj = new Place(place);
 		var title = newObj.name;
-		var errFlag = false;
+		
 
 		// Getting the geocode for the place.
 		geocoder.geocode({ 'address': place.address }, function(results, status) {
@@ -159,36 +160,36 @@ var createMap = function () {
 }
 
 function initMap() {
-google.maps.event.addDomListener(window, 'load', function(){
+	google.maps.event.addDomListener(window, 'load', function(){
 
-	// list of my places.
-	var myPlaces = [
-	{
-		name: 'The Albert',
-		address: '185 Walton Breck Rd, Liverpool L4 0RE, UK'
-	},
-	{
-		name: 'Arkles',
-		address: '77 Anfield Rd, Liverpool L4 0TJ, UK'
-	},
-	{
-		name: 'The Sandon',
-		address: '178-182 Oakfield Rd, Liverpool L4 0UH, UK'
-	},
-	{
-		name: 'The Park Pub',
-		address: '216-218 Walton Breck Rd, Liverpool L4 0RQ, UK'
-	},
-	{
-		name: 'The Twelfth Man',
-		address: '121 Walton Breck Rd, Liverpool L4 0RD, UK'
-	}
-	];
-	var googleMap = createMap();
-	var infoWindow = new google.maps.InfoWindow();
-	var bounds = new google.maps.LatLngBounds();
-	ko.applyBindings(new ViewModel(googleMap, myPlaces, infoWindow, bounds))
-});
+		// list of my places.
+		var myPlaces = [
+		{
+			name: 'The Albert',
+			address: '185 Walton Breck Rd, Liverpool L4 0RE, UK'
+		},
+		{
+			name: 'Arkles',
+			address: '77 Anfield Rd, Liverpool L4 0TJ, UK'
+		},
+		{
+			name: 'The Sandon',
+			address: '178-182 Oakfield Rd, Liverpool L4 0UH, UK'
+		},
+		{
+			name: 'The Park Pub',
+			address: '216-218 Walton Breck Rd, Liverpool L4 0RQ, UK'
+		},
+		{
+			name: 'The Twelfth Man',
+			address: '121 Walton Breck Rd, Liverpool L4 0RD, UK'
+		}
+		];
+		var googleMap = createMap();
+		var infoWindow = new google.maps.InfoWindow();
+		var bounds = new google.maps.LatLngBounds();
+		ko.applyBindings(new ViewModel(googleMap, myPlaces, infoWindow, bounds))
+	});
 }
 
 function populateinfoWindow(marker, infoWindow) {
